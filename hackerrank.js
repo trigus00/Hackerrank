92781229674ba4d197d30b8ab35ef7e55cbb3af2
Gustavo Mendoza <trigus00@gmail.com>
2022-01-11 16:36:03 -0600

    starcase

--- hackerrank.js
+++ hackerrank.js
@@ -1,26 +1,13 @@
 // console.log('Hello world ')
-function diagonalDifference(arr) {
+
+function staircase(n) {
     // Write your code here
-   
-        // Write your code here
-        let right = 0 ;
-        let left = 0 
-        for (let i = 0 ; i< arr.length;i++){
-            for(let j = 0; j < arr.length;j++){
-               if(i===j){
-                    right+= arr[i][j]
-               }
-               if( i+j == arr.length -1 ) {
-                   left += arr[i][j]
-               }
-               
-            }
-            
-        }
-        // console.log(arr)
-        console.log(Math.abs(right-left))
+    let star = '#'
+    for (let i = 0; i< n; i++){
+        console.log(star.padStart(n))
+        star = '#' + star
+
     }
+}
 
-
-diagonalDifference([[11, 2, 4],[4, 5, 6], [10, 8, -12]])
-
+staircase(6)
