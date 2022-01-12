f03b279210f9897614540a8d912166fc3cd7dbad
Gustavo Mendoza <trigus00@gmail.com>
2022-01-11 21:13:24 -0600

    Max and min !

--- hackerrank.js
+++ hackerrank.js
@@ -1,13 +1,20 @@
 // console.log('Hello world ')
 
-function staircase(n) {
-    // Write your code here
-    let star = '#'
-    for (let i = 0; i< n; i++){
-        console.log(star.padStart(n))
-        star = '#' + star
 
-    }
+function miniMaxSum(arr) {
+    let max = 0;
+    let min = 0;
+    let total = 0;
+    
+    max = Math.max(...arr)
+    min = Math.min(...arr)
+
+    arr.reduce((previouseValue, currentValue) => total = currentValue + previouseValue, 0)
+    max = total - max;
+    min = total - min;
+    console.log(max, min)
+
 }
-
-staircase(6)
+miniMaxSum([256741038, 623958417, 467905213, 714532089, 938071625])
+// let total = [1,2,3,4,5].reduce((previouseValue,currentValue)=>currentValue+previouseValue,0)
+// console.log(total)
